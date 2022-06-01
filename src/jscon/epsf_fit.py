30f6dac27c225c89b496dacf3068b0bb32fedbd2
2ndmk2 <aizw.masa@gmail.com>
2022-06-01 11:10:03 +0800

    changed

--- src/jscon/epsf_fit.py
+++ src/jscon/epsf_fit.py
@@ -15,7 +15,25 @@ from photutils.psf import EPSFBuilder, extract_stars
 
 
 def main(image, x_select, y_select,  ext_size= 25, n_oversample = 2, n_maxiter=10, norm_radius = 5.5, recentering_boxsize =7):
+    """
+        epsf fitting for image given stellar positions (x_select, y_select). 
 
+ 
+        Params:
+            image: 2D image
+            x_select: Array for x-positions for stars in pixel coordinate
+            y_select: Array for y-positions for stars in pixel coordinate
+            ext_size: Cutout size for epsf models & centroid fiting ("ext_size" * "ext_size" pixels)
+            n_oversample: Oversampling rate for epsf building
+            n_maxiter: Maximum iteration for epsf fitting & centroid fitting
+            norm_radius: The radius inside which the ePSF is normalized by the sum over undersampled integer pixel values inside a circular aperture??
+            recentering_boxsize: The size (in pixels) of the box used to calculate the centroid of the ePSF during each build iteration. If a single integer number is provided, \
+                then a square box will be used. If two values are provided, then they should be in (ny, nx) order.
+        Returns:
+            dx: Array of difference of x-positions between estimated and reference positions
+            dy: Array of difference of x-positions between estimated and reference positions
+            epsf_image: 2D epsf image
+    """
     stars_tbl = Table()
     stars_tbl['x'] = x_select 
     stars_tbl['y'] = y_select
@@ -38,7 +56,7 @@ def main(image, x_select, y_select,  ext_size= 25, n_oversample = 2, n_maxiter=1
     pos_stars = fitted_stars.center_flat
     dx= pos_stars[:,0] - x_select
     dy= pos_stars[:,1] - y_select  
-    #plt.imshow(np.log10(epsf.data))
-    #plt.show()
-    return dx, dy, epsf.data
+    epsf_image = epsf.data 
+
+    return dx, dy, epsf_image
 
